d6cebee374fc7c89cbad3fc9ad04d7816a6a714a
Andrew Poydence <apoydence@pivotal.io>
2018-07-17 09:12:36 -0600

    More debugging

--- function.js
+++ function.js
@@ -1,3 +1,7 @@
+var finalhandler = require('finalhandler')
+var Router       = require('router')
+var router       = Router()
+
 exports.helloWorld = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello World!';
   res.status(200).send(message);
