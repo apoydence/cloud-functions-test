aef983bd78c255d55134b4aa864ced525e974acd
Andrew Poydence <apoydence@pivotal.io>
2018-07-16 19:50:10 -0600

    Adds cloudSQLTest function

--- function.js
+++ function.js
@@ -8,3 +8,24 @@ exports.helloWorld = (req, res) => {
       let message = req.query.message || req.body.message || 'Hello World!';
       res.status(200).send(message);
 };
+
+const pg = require('pg');
+
+const connectionName = process.env.CONN_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const pool = new pg.Pool({
+    max: 1,
+    host: '/cloudsql/' + connectionName,
+    user: dbUser,
+    password: dbPass,
+    database: dbName
+});
+
+exports.cloudSQLTest = function cloudSQLTest(event, callback) {
+    pool.query('SELECT NOW() as now', (error, results) => {
+      callback(error, results);
+    });
+};
