29a605f671a7237488d4a37f89c4ea73d344f628
Andrew Poydence <apoydence@pivotal.io>
2018-07-17 23:12:25 -0600

    require auth from photos and read from db

--- photos.js
+++ photos.js
@@ -1,23 +1,51 @@
+var finalhandler = require('finalhandler')
+var Router       = require('router')
+var router       = Router()
 
-const bucketName = process.env.BUCKET_NAME;
+router.use(require('./users').middleware);
+router.use((req, res, next) => {
+    if (!req.user) {
+        res.status(401).end();
+        return;
+    }
+
+    next();
+});
+
+router.get('/photos', (req, res) => {
+    pool.query('SELECT id,name FROM photos', (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(JSON.stringify({"error":"failed to make request to database"}));
+            return;
+        }
+
+        let photos = results.rows.map(row=> {
+            return {
+              id: row.id,
+              name: row.name,
+              links: [
+                  {rel: "self", method: "GET", href: "/photos/"+row.id},
+              ],
+            }
+        });
+
+        userSchema.photos=photos;
+        res.status(200).send(JSON.stringify(userSchema));
+    });
+});
+
+router.param('photo_id', (req, res, next, idStr) => {
+    let id = parseInt(idStr);
+    if (isNaN(id)){
+        next(new Error('photo id must be an integer'));
+        return;
+    }
+
+    req.photo_id=id;
+    next();
+});
 
 exports.photos = (req, res) => {
-    const Storage = require('@google-cloud/storage');
-
-    // Creates a client
-    const storage = new Storage();
-
-    storage
-        .bucket(bucketName)
-        .getFiles()
-        .then(results => {
-             const files = results[0];
-             res.status(200).json({
-                 "photos": files.map(f=>f.name),
-             });
-        })
-        .catch(err => {
-             console.error(err);
-             res.status(500).json({error:"failed to read bucket"});
-        });
+    router(req, res, finalhandler(req, res));
 };
