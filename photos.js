1b73612eb3eddadb1a1c38f8d24f780e34f3f007
Andrew Poydence <apoydence@pivotal.io>
2018-07-17 20:05:08 -0600

    make photos list bucket

--- photos.js
+++ photos.js
@@ -1,4 +1,23 @@
 
+const bucketName = process.env.BUCKET_NAME;
+
 exports.photos = (req, res) => {
-    res.status(200).json({"photos":[]});
+    const Storage = require('@google-cloud/storage');
+
+    // Creates a client
+    const storage = new Storage();
+
+    storage
+        .bucket(bucketName)
+        .getFiles()
+        .then(results => {
+             const files = results[0];
+             res.status(200).json({
+                 "photos": files.map(f=>f.name),
+             });
+        })
+        .catch(err => {
+             console.error(err);
+             res.status(500).json({error:"failed to read bucket"});
+        });
 };
